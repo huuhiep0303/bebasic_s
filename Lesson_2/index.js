90f687b0d8131db2926fe5cb974cfe090f665c9a
Hiep Cao Huu <cacaohh04@gmail.com>
2025-03-27 21:14:01 +0700

    finished lesson_2

--- Lesson_2/index.js
+++ Lesson_2/index.js
@@ -1,21 +1,9 @@
-// const express = require('express');
-// const app = express();
-
-// app.get('/hello', (req, res) => {
-//     res.json({ message: "Hello, world!" });
-// });
-
-// app.listen(3000, () => {
-//     console.log("Server is running on http://localhost:3000");
-// });
-
-
 const express = require('express')
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.status(202).send('World!')
 })
 
 app.listen(port, () => {
